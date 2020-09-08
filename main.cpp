ca6c3525a626bf82d8f1a2be746ab24461970c9a
elnokrashi <51987305+elnokrashi@users.noreply.github.com>
2020-09-08 14:21:17 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,13 +5,13 @@
 int main()
 {
 
-intval[] = { 60, 100, 120 };
+int val[] = { 60, 100, 120 };
 
-intwt[] = { 10, 20, 30 };
+int wt[] = { 10, 20, 30 };
 
-intW = 50;
+int W = 50;
 
-intn = sizeof(val) / sizeof(val[0]);
+int n = sizeof(val) / sizeof(val[0]);
 
 cout << knapSack(W, wt, val, n);
 
